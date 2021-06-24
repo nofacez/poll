56ea30d41cccfd77f198afe378ded921de20ab25
nofacez <genizzz@yandex.ru>
2021-06-24 11:28:54 +0300

    i18next setup

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from './locales/en.js';
+import ru from './locales/ru.js';
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
-      en,
+      ru,
     },
-    lng: 'en',
+    lng: 'ru',
   });
 
 export default i18n;
