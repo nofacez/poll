aef8c76cc763d104c9b62925052e623a31aab8dd
nofacez <genizzz@yandex.ru>
2021-06-24 11:29:08 +0300

    main files

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App.jsx';
 import './styles/style.css';
+import './i18n.js';
 
 render(<App />, document.querySelector('#main'));
