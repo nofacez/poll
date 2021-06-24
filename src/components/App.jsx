05167e48bfb87901b485b021223afd6f979e318a
nofacez <genizzz@yandex.ru>
2021-06-24 11:30:16 +0300

    main file added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Provider, useSelector } from 'react-redux';
+
+import getStore from '../app/store.js';
+import WelcomeCard from './WelcomeCard.jsx';
+import QuestionCard from './QustionCard.jsx';
 
 const App = () => {
-  const i = 'Welcome';
+  const store = getStore();
+  const Main = () => {
+    const { status } = useSelector((state) => state.questionsInfo);
+    console.log(status);
+    return (
+      <div className="container">
+        {status === 'init' && <WelcomeCard />}
+        {status === 'initialized' && <QuestionCard />}
+      </div>
+    );
+  };
+
   return (
-    <div className="container">
-      {i}
-    </div>
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 };
 
