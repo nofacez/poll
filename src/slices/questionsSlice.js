6be1a6b8431f0112c3b78de1d87bbf516c8bf763
nofacez <genizzz@yandex.ru>
2021-06-24 16:24:26 +0300

    new reducers

--- src/slices/questionsSlice.js
+++ src/slices/questionsSlice.js
@@ -6,16 +6,30 @@ export const quesitonsSlice = createSlice({
     status: 'init',
     currentQuestionId: null,
     questions: [],
+    currentAnswers: {},
   },
   reducers: {
-    getQuestions: (state, action) => ({ currentQuestionId: 0, questions: action.payload, status: 'initialized' }),
+    getQuestions: (state, action) => ({
+      ...state, currentQuestionId: 0, questions: action.payload, status: 'initialized',
+    }),
     setStatus: (state, action) => {
       const { payload } = action;
       return { ...state, status: payload };
     },
+    setCurrentQuestion: (state, action) => {
+      const { payload } = action;
+      return { ...state, currentQuestionId: payload.id };
+    },
+    addAnswers: (state, action) => {
+      const { payload: { questionId, answer } } = action;
+      const { currentAnswers } = state;
+      currentAnswers[questionId] = Array.isArray(answer) ? answer : [answer];
+    },
   },
 });
 
-export const { getQuestions, setStatus } = quesitonsSlice.actions;
+export const {
+  getQuestions, setStatus, setCurrentQuestion, addAnswers,
+} = quesitonsSlice.actions;
 
 export default quesitonsSlice.reducer;
