cd0eced54ec71705fada703d5d06cf379a4be158
nofacez <genizzz@yandex.ru>
2021-06-27 22:23:40 +0300

    dployment fixes

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import App from './components/App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.css';
+import 'regenerator-runtime/runtime';
 import './i18n.js';
 import 'babel-polyfill';
 
