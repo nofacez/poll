71b1865fd69dc2881c617e659e2017eea8937c94
nofacez <genizzz@yandex.ru>
2021-06-27 22:26:22 +0300

    fixes

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', {
-    targets: {
-      esmodules: true,
-    },
-  }],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: ['@babel/transform-runtime'],
 };
